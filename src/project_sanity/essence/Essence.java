2784f0c18fc50ec711df22d5197599d794b50877
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-24 21:00:52 -0400

    - Essence.java no longer implements IWoundable or IMendable

--- src/project_sanity/essence/Essence.java
+++ src/project_sanity/essence/Essence.java
@@ -1,24 +1,12 @@
 package project_sanity.essence;
 
-import project_sanity.IMendable;
-import project_sanity.IWoundable;
 import project_sanity.range.Range;
 
 /**
  *
  * @author nicholas malacarne <nicholas.malacarne@gmail.com>
  */
-public class Essence implements IWoundable, IMendable {
+public class Essence {
 
     private final Range range;
-    
-    @Override
-    public void wound() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void mend() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 }
