8bbef2ea73de9238ecfaad6de4159952e651756c
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-24 23:15:14 -0400

    - WoundPool now implements IWoundable and IMendable

--- src/project_sanity/pool/wound/WoundPool.java
+++ src/project_sanity/pool/wound/WoundPool.java
@@ -1,9 +1,21 @@
 package project_sanity.pool.wound;
 
+import project_sanity.IMendable;
+import project_sanity.IWoundable;
+
 /**
  *
  * @author nicholas malacarne <nicholas.malacarne@gmail.com>
  */
-public class WoundPool {
+public class WoundPool implements IWoundable, IMendable {
 
+    @Override
+    public void wound() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mend() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
