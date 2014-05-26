2493d48bee2c9ebc1e78a414e0176ad7a17a3d89
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-26 01:17:47 -0400

    Revert "- WoundPool now implements IWoundable and IMendable"
    This reverts commit 8bbef2ea73de9238ecfaad6de4159952e651756c.

--- src/project_sanity/pool/wound/WoundPool.java
+++ src/project_sanity/pool/wound/WoundPool.java
@@ -1,21 +1,9 @@
 package project_sanity.pool.wound;
 
-import project_sanity.IMendable;
-import project_sanity.IWoundable;
-
 /**
  *
  * @author nicholas malacarne <nicholas.malacarne@gmail.com>
  */
-public class WoundPool implements IWoundable, IMendable {
+public class WoundPool {
 
-    @Override
-    public void wound() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void mend() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 }
