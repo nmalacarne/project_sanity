2bb9ea6954e5f1643f765f35f31c26f93b77fedc
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-27 14:48:22 -0400

    - added class Energy

---
+++ src/project_sanity/essence/energy/Energy.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project_sanity.essence.energy;
+
+import project_sanity.essence.AEssence;
+import project_sanity.range.Range;
+
+/**
+ *
+ * @author nicholas malacarne <nicholas.malacarne@gmail.com>
+ */
+public class Energy extends AEssence {
+
+    private final Range range;
+
+    public Energy() {
+        this.range = new Range(0, 10);
+    }
+    
+    @Override
+    public void decay() {
+        if (super.getValue() < range.getMAX()){
+            super.decay();
+        }else{
+            // alert energy empty -nm
+        }
+    }
+
+    @Override
+    public void grow() {
+        if (super.getValue() > range.getMIN()){
+            super.grow();
+        }else{
+            // alert energy full -nm
+        }
+    }
+    
+    @Override
+    public int getValue() {
+        int total = this.range.getMAX() - super.getValue();
+        return range.rangeCheck(total);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
